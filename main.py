e0fe4f53f33bb7b8cc293da25f95359785e6c7f2
Seran Shanmugathas <seran@live.no>
2023-04-15 19:24:16 +0200

    feat: run main file based on args

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 '''
 Main file for the Nim game.
 '''
+import sys
+from config.neural_network import INPUT_SHAPE, OUTPUT_SHAPE, LAYERS, ACTIVATION, OPTIMIZER, LEARNING_RATE
 from game import Hex
 from mcts import MCTS, Node
-from neural_network.anet import ANet
+from neural_network.anet import ANet, load_model
 from reinforcement_learning.agent import Agent
-from config.neural_network import INPUT_SHAPE, OUTPUT_SHAPE, LAYERS, ACTIVATION, OPTIMIZER, LEARNING_RATE
 
 
 def main():
@@ -37,18 +38,19 @@ def main():
         print("Human wins!")
 
 
-# if __name__ == "__main__":
-    # main()
-
 if __name__ == "__main__":
-    agent = Agent()
-    agent.run()
-
-# if __name__ == "__main__":
-#     anet = ANet(input_shape=INPUT_SHAPE,
-#                 output_shape=OUTPUT_SHAPE,
-#                 layers=LAYERS,
-#                 activation=ACTIVATION,
-#                 optimizer=OPTIMIZER,
-#                 learning_rate=LEARNING_RATE)
-#     print(anet.predict([[0]*25]))
+    if sys.argv[1] == "train":
+        anet = ANet(input_shape=INPUT_SHAPE,
+                    output_shape=OUTPUT_SHAPE,
+                    layers=LAYERS,
+                    activation=ACTIVATION,
+                    optimizer=OPTIMIZER,
+                    learning_rate=LEARNING_RATE)
+        # load_model(anet)
+        agent = Agent(anet=anet)
+        agent.run(use_neural_network=True)
+    elif sys.argv[1] == "play":
+        agent = Agent()
+        agent.run()
+    else:
+        main()
