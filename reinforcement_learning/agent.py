660a3a5de0b229c234c7d140673f7ec5d993e679
Seran Shanmugathas <seran@live.no>
2023-04-15 19:16:20 +0200

    chore: run with or without nn based on bool value

--- reinforcement_learning/agent.py
+++ reinforcement_learning/agent.py
@@ -1,13 +1,11 @@
+'''
+This module contains the reinforcement learning algorithm
+'''
 import random
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-
-from game import State
 from game.hex.hex import Hex
+from mcts import MCTS
 from mcts.node import Node
 from neural_network.anet import ANet
-from mcts import MCTS
 
 
 class ReplayBuffer:
@@ -54,62 +52,52 @@ class Agent:
     Agent class
     '''
 
-    # def __init__(self, anet: ANet, replay_buffer: ReplayBuffer, save_interval, number_actual_games, number_search_games):
-    #     self.anet = anet
-    #     self.replay_buffer = replay_buffer
-    #     self.save_interval = save_interval
-    #     self.number_actual_games = number_actual_games
-    #     self.number_search_games = number_search_games
+    def __init__(
+            self,
+            anet: ANet = None,
+            replay_buffer: ReplayBuffer = None,
+            save_interval=None,
+            number_actual_games=None,
+            number_search_games=None
+    ):
+        self.anet = anet or None
+        self.replay_buffer = replay_buffer or ReplayBuffer(1000)
+        self.save_interval = save_interval or 100
+        self.number_actual_games = number_actual_games or 1000
+        self.number_search_games = number_search_games or 100
 
-    def __init__(self) -> None:
-        self.replay_buffer = ReplayBuffer(1000)
-        self.save_interval = 100
-        self.number_actual_games = 1000
-        self.number_search_games = 100
-
-    def is_final_state(self, state):
-        # Implement this function according to your specific problem and design
-        pass
-
-    def backpropagate(self, mcts, final_state):
-        # Implement this function according to your specific problem and design
-        pass
-
-    # def train_anet(self, minibatch):
-    #     '''
-    #     Train the agent's neural network
-
-    #     Parameters
-    #     ----------
-    #     minibatch : list
-    #         A minibatch of cases
-    #     '''
-    #     self.anet.train(minibatch)
-
-    def run(self):
+    def run(self, use_neural_network: bool = False):
         '''
         Run the agent
         '''
         for actual_game in range(self.number_actual_games):
-            game = Hex(5)
-            root_node = Node(game)
-            # FIXME: Uncomment the following line to use the MCTS with the neural network
-            # mcts = MCTS(root_node, 100, self.anet)
-            mcts = MCTS(root_node, 100)
+            if use_neural_network:
+                game = Hex(5)
+                root_node = Node(game)
+                mcts = MCTS(root_node, 100, self.anet)
 
-            while not game.is_terminal():
-                print(game.board)
-                best_child, distribution = mcts()
-                self.replay_buffer.add_case((mcts.root_node, distribution))
-                action = best_child.state.get_previous_action()
-                print("AI move: ", action)
-                mcts.root_node.state.produce_successor_state(action)
-                mcts.root_node = (Node(mcts.root_node.state))
+                while not game.is_terminal():
+                    print(game.board)
+                    best_child, distribution = mcts()
+                    self.replay_buffer.add_case((mcts.root_node, distribution))
+                    action = best_child.state.get_previous_action()
+                    print("AI move: ", action)
+                    mcts.root_node.state.produce_successor_state(action)
+                    mcts.root_node = Node(mcts.root_node.state)
 
-            # FIXME: Uncomment the following line to use the MCTS with the neural network
-            # minibatch = self.replay_buffer.sample_minibatch()
-            # self.train_anet(minibatch)
+                minibatch = self.replay_buffer.sample_minibatch()
+                self.anet.train(minibatch)
 
-            # FIXME: Uncomment the following line to use the MCTS with the neural network
-            # if actual_game % self.save_interval == 0:
-            #     self.anet.save()
+                if actual_game % self.save_interval == 0:
+                    self.anet.save()
+            else:
+                mcts = MCTS(root_node, 100)
+
+                while not game.is_terminal():
+                    print(game.board)
+                    best_child, distribution = mcts()
+                    self.replay_buffer.add_case((mcts.root_node, distribution))
+                    action = best_child.state.get_previous_action()
+                    print("AI move: ", action)
+                    mcts.root_node.state.produce_successor_state(action)
+                    mcts.root_node = Node(mcts.root_node.state)
