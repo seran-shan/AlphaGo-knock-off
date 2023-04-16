9418b77a8b7e5ad0936430eb60a3314d7d8a2d12
Seran Shanmugathas <seran@live.no>
2023-04-16 13:53:49 +0200

    refactor: new main method

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 '''
 Main file for the Nim game.
 '''
-import sys
-from config.neural_network import INPUT_SHAPE, OUTPUT_SHAPE, LAYERS, ACTIVATION, OPTIMIZER, LEARNING_RATE
+from config import INPUT_SHAPE, OUTPUT_SHAPE, LAYERS, ACTIVATION, OPTIMIZER, LEARNING_RATE
 from game import Hex
 from mcts import MCTS, Node
-from neural_network.anet import ANet, load_model
-from reinforcement_learning.agent import Agent
+from neural_network import ANet
+from reinforcement_learning import Agent
 
 
 def main():
@@ -39,18 +38,12 @@ def main():
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "train":
-        anet = ANet(input_shape=INPUT_SHAPE,
-                    output_shape=OUTPUT_SHAPE,
-                    layers=LAYERS,
-                    activation=ACTIVATION,
-                    optimizer=OPTIMIZER,
-                    learning_rate=LEARNING_RATE)
-        # load_model(anet)
-        agent = Agent(anet=anet)
-        agent.run(use_neural_network=True)
-    elif sys.argv[1] == "play":
-        agent = Agent()
-        agent.run()
-    else:
-        main()
+    anet = ANet(input_shape=INPUT_SHAPE,
+                output_shape=OUTPUT_SHAPE,
+                layers=LAYERS,
+                activation=ACTIVATION,
+                optimizer=OPTIMIZER,
+                learning_rate=LEARNING_RATE)
+    # load_model(anet)
+    agent = Agent(anet=None)
+    agent.run(use_neural_network=False)
