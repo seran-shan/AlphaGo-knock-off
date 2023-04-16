409952a3623cdd386a3cd71fd26d948c62527c8f
Seran Shanmugathas <seran@live.no>
2023-04-16 16:42:14 +0200

    fix: typo in state representation

--- game/hex/hex.py
+++ game/hex/hex.py
@@ -53,7 +53,7 @@ class State:
         """
         pass
 
-    def extract_represenation(self):
+    def extract_representation(self):
         """
         Return the representation of the state
 
@@ -306,12 +306,16 @@ class Hex:
     def extract_representation(self):
         '''
         Extract a representation of the current state, to feed it to a neural network.
-
         '''
         flat_board = self.board.flatten()
         player_to_move = np.array([self.player if self.player == 1 else -1])
-        last_move = np.array([self.last_move[0] * self.size +
-                             self.last_move[1] if self.last_move is not None else [-1]])
+
+        # if self.last_move is not None:
+        #     last_move = np.array(
+        #         [self.last_move[0] * self.size + self.last_move[1]])
+        # else:
+        #     last_move = np.array([-1])
+
         board_representation = np.hstack(
-            [flat_board, player_to_move, last_move])
+            [flat_board, player_to_move])
         return board_representation
