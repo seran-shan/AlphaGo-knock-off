97629f1055b3cd8579fed622e0e0cb2c0b2383bd
Ferdinand Eide <ferdinae@stud.ntnu.no>
2023-04-17 20:29:53 +0200

    fix: config changes

--- config/reinforcement_learning.py
+++ config/reinforcement_learning.py
@@ -3,6 +3,6 @@ This file contains the configuration for the reinforcement learning algorithm.
 '''
 REPLAY_BUFFER_SIZE = 100
 SAVE_INTERVAL = 10
-NUMBER_ACTUAL_GAMES = 10
-NUMBER_SEARCH_GAMES = 50
+NUMBER_ACTUAL_GAMES = 11
+SIMULATIONS = 1000
 IDENTIFIER = 'model'
