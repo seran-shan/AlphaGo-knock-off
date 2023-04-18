80919a6afe2fbaf94b9205097293aa552779e0aa
Seran Shanmugathas <seran@live.no>
2023-04-18 18:02:49 +0200

    chore: remove counter

--- config/reinforcement_learning.py
+++ config/reinforcement_learning.py
@@ -2,7 +2,7 @@
 This file contains the configuration for the reinforcement learning algorithm.
 '''
 REPLAY_BUFFER_SIZE = 100
-SAVE_INTERVAL = 10
+SAVE_INTERVAL = 5
 NUMBER_ACTUAL_GAMES = 11
 SIMULATIONS = 1000
 IDENTIFIER = 'model'
