59461444bf4449103fe08f20065d438beb15e4a0
Seran Shanmugathas <seran@live.no>
2023-04-19 10:33:19 +0200

    chore: move all config to same file

---
+++ config/parameters.py
@@ -0,0 +1,28 @@
+'''
+General configuration
+'''
+BOARD_SIZE = 5
+TIME_LIMIT = 3
+DATE = '04-18'
+
+
+'''
+Configuration of neural network model
+'''
+INPUT_SHAPE = (BOARD_SIZE * BOARD_SIZE + 1)
+OUTPUT_SHAPE = (BOARD_SIZE * BOARD_SIZE)
+LAYERS = [256, 512]
+ACTIVATION = 'relu'
+OPTIMIZER = 'adam'
+LEARNING_RATE = 1e-3
+EPOCHS = 100
+BATCH_SIZE = 32
+
+'''
+This file contains the configuration for the reinforcement learning algorithm.
+'''
+REPLAY_BUFFER_SIZE = 100
+SAVE_INTERVAL = 20
+NUMBER_ACTUAL_GAMES = 10000
+SIMULATIONS = 10000
+IDENTIFIER = 'model'
--- config/reinforcement_learning.py
+++
@@ -1,8 +0,0 @@
-'''
-This file contains the configuration for the reinforcement learning algorithm.
-'''
-REPLAY_BUFFER_SIZE = 100
-SAVE_INTERVAL = 5
-NUMBER_ACTUAL_GAMES = 11
-SIMULATIONS = 1000
-IDENTIFIER = 'model'
